bdedb15c1e41835926f4bfd26010320a595f885b
Alisson1406 <0001005066@senaimgaluno.com.br>
2024-03-18 20:21:36 -0300

    index

--- paginaWeb/servidorNuvem/index.js
+++ paginaWeb/servidorNuvem/index.js
@@ -1,1 +1,30 @@
-//teste
+const express = require("express");
+var bodyParser = require("body-parser");
+const app = express();
+var jsonParser = bodyParser.json();
+
+var cors = require("cors");
+const { write } = require("fs");
+app.use(cors());
+
+var bit = 0;
+
+app.post("/", jsonParser, function (req, res) {
+  res.writeHead(200, { "Content-Type": "application/json", mode: "cors" });
+
+  bit = req.body.l1;
+  console.log(bit);
+  res.end();
+});
+app.get("/", function (req, res){
+  res.writeHead(200, { "Content-Type": "application/json", mode: "cors" });
+  res.write(
+    JSON.stringify({
+      l1: bit,
+    }),
+    );
+  res.end();
+});
+
+app.listen(3000);
+
